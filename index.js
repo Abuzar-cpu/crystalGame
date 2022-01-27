0da2067e0d3a71d392fc35e661a7566bedc4e1ec
Abuzar-cpu <kerimovabuzer25@gmail.com>
2022-01-27 14:34:50 +0400

    game functions partially formed

---
+++ index.js
@@ -0,0 +1,55 @@
+let totalScore = 0;
+let wins = 0;
+let loses = 0;
+
+crystals = $("button");
+
+goalScore = Math.ceil(Math.random()*100);
+$("#goal-score").text(goalScore);
+
+powers = [Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10), Math.ceil(Math.random()*10)];
+
+for (const button of crystals) {
+    $(button).attr("data-power", Math.ceil(Math.random()*10));
+}
+
+calcScore = (element) =>
+{
+    let temp = totalScore;
+    totalScore += parseInt($(element).data("power"));
+    if(checkIfGameOver() === 0)
+    {
+        alert("You won");
+        totalScore = temp;
+        updateScore(goalScore);
+        return;
+    }
+
+    else if(checkIfGameOver() === 1)
+    {
+        alert("You lost");
+        return;
+    }
+    updateScore(totalScore);
+
+}
+
+updateScore = (score) =>
+{
+    $("#user-score").text(score);
+}
+
+checkIfGameOver = () =>
+{
+    if(totalScore === goalScore)
+    {
+        return 0; // Means User won the game
+    }
+
+    else if(totalScore < goalScore)
+    {
+        return -1; // Means the game should continue
+    }
+
+    return 1; // Means User lost the game
+}
